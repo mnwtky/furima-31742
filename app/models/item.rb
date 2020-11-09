ea515a531751daa515f5237493ff71cf524d5e66
mnwtky <69100365+mnwtky@users.noreply.github.com>
2020-11-10 01:01:41 +0900

    商品保存機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
-  belongs_to :condition
-  belongs_to :postage_payer
-  belongs_to :prefecture
-  belongs_to :handling
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :handling
 
   validates :name, :description, presence: true
   with_options numericality: { other_than: 1} do
